12dd9a8c4c5a8d0ecc65fc4310ab775107efbc88
Robert <osceolarob@gmail.com>
2025-02-24 18:58:09 -0500

    expose IPC function

--- apps/desktop/src/preload.js
+++ apps/desktop/src/preload.js
@@ -4,16 +4,22 @@ import { contextBridge, ipcRenderer } from 'electron';
 // Expose protected methods that allow the renderer process to use
 // the ipcRenderer without exposing the entire object
 contextBridge.exposeInMainWorld('electron', {
-// Get list of local videos
+	// Get list of local videos
 	getLocalVideos: () => ipcRenderer.invoke('get-local-videos'),
 
-// Trigger video recording
+	// Trigger video recording
 	triggerRecordVideo: () => ipcRenderer.invoke('trigger-record'),
 
-// Remove specific video
+	// Remove specific video
 	removeSpecificVideo: (filename) => ipcRenderer.invoke('remove-specific-video', filename),
 
-// Listen for new recordings
+	// Get video metadata
+	getVideoMetadata: (filename) => ipcRenderer.invoke('get-video-metadata', filename),
+
+	// Save edited video
+	saveEditedVideo: (params) => ipcRenderer.invoke('save-edited-video', params),
+
+	// Listen for new recordings
 	onNewRecording: (callback) => 
 		ipcRenderer.on('new-recording', (event, data) => callback(data))
 });
