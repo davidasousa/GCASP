b8a68abf072be9baa2f6fc5c84b1d8ee7d0e388c
1094357992 <p1094357992@gmail.com>
2025-04-07 16:48:13 -0400

    API Development

--- apps/backend/server.js
+++ apps/backend/server.js
@@ -4,14 +4,17 @@ const cors = require("cors");
 const { sequelize } = require("./models");
 const friendRoutes = require("./routes/friends");
 const setupSwagger = require("./swagger");
+const authRoutes = require("./routes/auth");
+const videoRoutes = require("./routes/videos");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use("/auth", authRoutes);
+app.use("/videos", videoRoutes);
+app.use("/friends", friendRoutes);
 
 setupSwagger(app);
-
-app.use("/friends", friendRoutes);
 
 const PORT = process.env.PORT || 5001;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
