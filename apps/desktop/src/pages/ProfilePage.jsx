78c9b661ec7e4152d99ff83cff53fdea95ea16ad
davidasousa <davidasousa335@gmail.com>
2025-04-13 17:02:41 -0400

    progress

---
+++ apps/desktop/src/pages/ProfilePage.jsx
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from 'react';
+import '../styles/login-page.css';
+
+const ProfilePage = () => {
+    return (
+        <div className="profile-page">
+            Profile
+        </div>
+    );
+};
+
+export default ProfilePage;
